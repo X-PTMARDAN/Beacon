1990d14daa0891f2e5a753c26e460371b8e97eca
Shubham Aggarwal <shubham.aggarwal2020@gmail.com>
2019-08-14 00:42:16 +0530

    add modal

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import * as CanvasJS from './../../../assets/js/canvasjs.min';
 
 @Component({
@@ -7,10 +7,32 @@ import * as CanvasJS from './../../../assets/js/canvasjs.min';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+  public minEndWeek: string;
 
-  constructor() { }
+  // Planning Horizon
+  public startWeek: string;
+  public endWeek: string;
 
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    const currentDate = new Date();
+    this.startWeek = currentDate.getFullYear() + '-W' + DashboardComponent.getCurrentWeek(currentDate);
+    currentDate.setDate(currentDate.getDate() + 7);
+    this.minEndWeek = currentDate.getFullYear() + '-W' + DashboardComponent.getCurrentWeek(currentDate);
+  }
+
+  private static getCurrentWeek(date: Date) {
+    const firstDayOfYear = new Date(date.getFullYear(), 0, 1);
+    const pastDaysOfYear = (date.getTime() - firstDayOfYear.getTime()) / 86400000;
+    return Math.ceil((pastDaysOfYear + firstDayOfYear.getDay() + 1) / 7);
+  }
+
+  public addWeeks(numOfWeeks: number) {
+    const currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 7 * numOfWeeks);
+    this.endWeek = currentDate.getFullYear() + '-W' + DashboardComponent.getCurrentWeek(currentDate);
+  }
 }
+
