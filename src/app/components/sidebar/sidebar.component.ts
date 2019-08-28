5b49432cf385a83bab6c1334beaa03d514748d75
sad <harshitsharma0003@gmail.com>
2019-08-28 22:53:56 +0530

    commit

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {SidebarService} from '../../services/sidebar.service';
 
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
