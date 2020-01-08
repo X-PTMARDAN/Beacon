2b5a1d750bbfaebf1b8ba0d1cacf3b4c5cf06a8d
sad <harshitsharma0003@gmail.com>
2020-01-08 19:10:23 +0530

    ds
    dsf

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiGatewayUrl: 'https://ygroup-aurora.azurewebsites.net/v1/',
+  apiGatewayUrl: 'http://63.33.46.20:8119/v1/',
 };
 
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiGatewayUrl: 'https://ygroup-aurora.azurewebsites.net/v1/',
+  apiGatewayUrl: 'http://63.33.46.20:8119/v1/',
 };
 
 
