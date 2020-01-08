ba598de3a13b7394194d8c45119a7954b48838e4
sad <harshitsharma0003@gmail.com>
2020-01-08 20:13:59 +0530

    asd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiGatewayUrl: 'https://aurora-springboot.azurewebsites.net/v1/fetchHorizon',
+  apiGatewayUrl: 'https://aurora-springboot.azurewebsites.net/v1/',
 };
 
 
