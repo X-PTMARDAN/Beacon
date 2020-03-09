7293da58901d53a14983e63e163eb31e39a477df
ygroup-carlsberg <56654248+ygroup-carlsberg@users.noreply.github.com>
2020-03-09 19:07:19 +0530

    Add files via upload

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -24,8 +24,9 @@ export class SidebarComponent implements OnInit {
   ngOnInit() {
 
 
-
-    if(window.location.href=='http://aurora-testingcarlsberg.herokuapp.com/portfolio')
+    window.alert(window.location.hostname);
+    
+    if(window.location.href==window.location.hostname+'/portfolio')
     {
       document.getElementById("plan").style.background='#5A5F66';
       document.getElementById("portfolio").style.background='#797F87';
@@ -34,7 +35,7 @@ export class SidebarComponent implements OnInit {
       document.getElementById("npd").style.background='#5A5F66';
       document.getElementById("planscene").style.background='#5A5F66';
     }
-    else if(window.location.href=='http://aurora-testingcarlsberg.herokuapp.com/comingsoon?b=')
+    else if(window.location.href==window.location.hostname+'/comingsoon?b=')
     {
       document.getElementById("npd").style.background='#797F87';
       document.getElementById("portfolio").style.background='#5A5F66';
@@ -44,7 +45,7 @@ export class SidebarComponent implements OnInit {
       document.getElementById("planscene").style.background='#5A5F66';
     }
 
-    else if(window.location.href=='http://aurora-testingcarlsberg.herokuapp.com/comingsoon?bcd=')
+    else if(window.location.href=='http://france-aurora.azurewebsites.net/comingsoon?bcd=')
     {
       document.getElementById("npd").style.background='#5A5F66';
       document.getElementById("portfolio").style.background='#5A5F66';
@@ -53,7 +54,7 @@ export class SidebarComponent implements OnInit {
       document.getElementById("snop").style.background='#5A5F66';
       document.getElementById("planscene").style.background='#797F87';
     }
-    else if(window.location.href=='http://aurora-testingcarlsberg.herokuapp.com/comingsoon?a=')
+    else if(window.location.href=='http://france-aurora.azurewebsites.net/comingsoon?a=')
     {
       document.getElementById("snop").style.background='#797F87';
       document.getElementById("portfolio").style.background='#5A5F66';
@@ -62,7 +63,7 @@ export class SidebarComponent implements OnInit {
       document.getElementById("npd").style.background='#5A5F66';
       document.getElementById("planscene").style.background='#5A5F66';
     }
-    else if(window.location.href=='http://aurora-testingcarlsberg.herokuapp.com/setting')
+    else if(window.location.href=='http://france-aurora.azurewebsites.net/setting')
     {
       document.getElementById("snop").style.background='#5A5F66';
       document.getElementById("portfolio").style.background='#5A5F66';
@@ -97,9 +98,9 @@ export class SidebarComponent implements OnInit {
   public onClick(name: string) {
     console.log("Harshit-->"+name);
     console.log("YHYHY-->"+window.location.href);
-    if(window.location.href=="http://aurora-testingcarlsberg.herokuapp.com/portfolio?%2F=")
+    if(window.location.href=="http://france-aurora.azurewebsites.net/portfolio?%2F=")
     {
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/dashboard";
+      window.location.href="http://france-aurora.azurewebsites.net/dashboard";
     }
     this.sidebarService.emit(name);
   }
@@ -187,30 +188,30 @@ export class SidebarComponent implements OnInit {
     if(pageName=='a')
     {
       this.num=1;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/comingsoon?a"
+      window.location.href="http://france-aurora.azurewebsites.net/comingsoon?a"
      // this.router.navigate([`/portfolio`]);
     }
     else if(pageName=='b')
     {
       this.num=2;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/comingsoon?b"
+      window.location.href="http://france-aurora.azurewebsites.net/comingsoon?b"
     }
 
     else if(pageName=='bcd')
     {
       this.num=56;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/comingsoon?bcd"
+      window.location.href="http://france-aurora.azurewebsites.net/comingsoon?bcd"
     }
 
     else if(pageName=='ab')
     {
       this.num=2;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/portfolio"
+      window.location.href="http://france-aurora.azurewebsites.net/portfolio"
     }
     else if(pageName=='c')
     {
       this.num=3;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/dashboard"
+      window.location.href="http://france-aurora.azurewebsites.net/dashboard"
     }
     else if(pageName=='d')
     {
@@ -221,7 +222,7 @@ export class SidebarComponent implements OnInit {
     else if(pageName=='e')
     {
       this.num=5;
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/setting"
+      window.location.href="http://france-aurora.azurewebsites.net/setting"
     }
 
 
@@ -229,7 +230,7 @@ export class SidebarComponent implements OnInit {
     {
       this.num=5;
       sessionStorage.removeItem('username');
-      window.location.href="http://aurora-testingcarlsberg.herokuapp.com/login"
+      window.location.href="http://france-aurora.azurewebsites.net/login"
     }
 
     
