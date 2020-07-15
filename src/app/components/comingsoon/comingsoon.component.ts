e145f038119f1503f2d0168a3e2893ac4b0ca8a7
ygroup-carlsberg <56654248+ygroup-carlsberg@users.noreply.github.com>
2020-07-15 04:38:13 -0400

    Add files via upload

--- src/app/components/comingsoon/comingsoon.component.ts
+++ src/app/components/comingsoon/comingsoon.component.ts
@@ -12,4 +12,41 @@ export class ComingsoonComponent implements OnInit {
   ngOnInit() {
   }
 
+  public iframe=true;
+  public link=true;
+
+  public fourth=false;
+  public third=false;
+
+  public second=false;
+
+  public first=false;
+
+
+  public graph(src1)
+  {
+    this.iframe=false;
+    this.link=true;
+    this.first=false;
+    this.second=false;
+    this.third=false;
+    this.fourth=false;
+    if(src1=='a')
+    {
+      this.first=true;
+    }
+    else if(src1=='b')
+    {
+      this.second=true;
+    }
+    else if(src1=='c')
+    {
+this.third=true;
+    }
+    else if(src1=='d')
+    {
+      this.fourth=true;
+    }
+  }
+
 }
