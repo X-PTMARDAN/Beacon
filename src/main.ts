c0de66cd37d58d399ec97ab03de21c02c5b5714f
ygroup-carlsberg <56654248+ygroup-carlsberg@users.noreply.github.com>
2020-11-20 14:49:45 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
-import {LicenseManager} from "ag-grid-enterprise";
+import {LicenseManager} from "ag-grid-enterprise"; //20nov - purchased ag grid enterprise
 LicenseManager.setLicenseKey("CompanyName=YStrategists B.V.,LicensedGroup=YGroup - DCD GRID,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=2,AssetReference=AG-008806,ExpiryDate=7_July_2021_[v2]_MTYyNTYxMjQwMDAwMA==1b8d7ca822a08e10928e771bdaa0d469");
 
 if (environment.production) {
