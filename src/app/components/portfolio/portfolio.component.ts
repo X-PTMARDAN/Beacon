d2517c045544762bba24fdb2f2e8ab0ed933086f
ygroup-carlsberg <56654248+ygroup-carlsberg@users.noreply.github.com>
2020-12-07 10:02:55 +0530

    Add files via upload

--- src/app/components/portfolio/portfolio.component.ts
+++ src/app/components/portfolio/portfolio.component.ts
@@ -475,11 +475,12 @@ export class PortfolioComponent implements OnInit, OnDestroy {
         
       });
       
-
+      /*
       this.skuService.getCVC().subscribe((response: any) => {
         this.cvcData = response;
         this.cvcDataAll = response;
       });
+      */
   
       this.skuService.getmaxweek().subscribe((response: any) => {
         this.maxweek = response;
