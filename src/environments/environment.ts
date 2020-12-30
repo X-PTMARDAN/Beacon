683c2b17bc40fbf24e16de2149ba28fced748fce
ygroup-carlsberg <56654248+ygroup-carlsberg@users.noreply.github.com>
2020-12-30 12:57:27 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiGatewayUrl: 'https://aurora-springboot.azurewebsites.net/v1/',  //france backend
+  apiGatewayUrl: 'https://norway-springboot.azurewebsites.net/v1/',  //france backend
 };
 
 
